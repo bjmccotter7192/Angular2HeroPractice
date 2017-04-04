df1584214d9ac56c48a95d657562e7250795caf5
William McCotter <BJ.McCotter@vu.com>
2017-04-04 15:59:19 -0500

    Moved Files Around

--- HeroesPractice/app/app.module.ts
+++ HeroesPractice/app/app.module.ts
@@ -1,19 +1,27 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
 import { AppRoutingModule } from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './Heroes/hero-details.component';
-import { HeroService } from './Heroes/hero.service';
+import { HeroDetailComponent } from './hero-details.component';
+import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
 
+
 @NgModule({
-  imports:[ 
+  imports:[
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule    
   ],
   declarations: [
      AppComponent,
@@ -23,6 +31,6 @@ import { DashboardComponent } from './dashboard.component';
   ],
   providers:[ HeroService ],
   bootstrap:[ AppComponent ],
-  
+
 })
 export class AppModule { }
