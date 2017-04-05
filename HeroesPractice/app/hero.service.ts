da1a01d3928245688daadfb1f8ccaec517420fae
William McCotter <BJ.McCotter@vu.com>
2017-04-05 10:01:30 -0500

    Update, Delete, and Add New Heroes

--- HeroesPractice/app/hero.service.ts
+++ HeroesPractice/app/hero.service.ts
@@ -7,6 +7,7 @@ import 'rxjs/add/operator/toPromise';
 export class HeroService {
 
   private heroesUrl = 'api/heroes';
+  private headers = new Headers({'Content-Type': 'application/json'});
 
   constructor(private http: Http) { }
 
@@ -28,5 +29,29 @@ export class HeroService {
         .toPromise()
         .then(response => response.json().data as Hero)
         .catch(this.handleError);
+  }
+
+  update(hero: Hero): Promise<Hero>{
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http
+      .put(url, JSON.stringify(hero), {headers: this.headers})
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  create(name: string): Promise<Hero>{
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Hero)
+      .catch(this.handleError);
+  }
+
+  delete(id: number): Promise<void>{
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
   }
 }
